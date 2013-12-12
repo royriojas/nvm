e98737cc1dd537a2e2301eef58db15d52d8340aa
tjwebb <travis@traviswebb.com>
2013-12-12 13:28:26 -0500

    install globally

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NVM_DIR="$HOME/.nvm"
+NVM_DIR="/usr/local/nvm"
 
 if ! hash git 2>/dev/null; then
   echo >&2 "You need to install git - visit http://git-scm.com/downloads"
@@ -14,52 +14,8 @@ if [ -d "$NVM_DIR" ]; then
   cd $NVM_DIR && git pull
 else
   # Cloning to $NVM_DIR
-  git clone https://github.com/creationix/nvm.git $NVM_DIR  
+  git clone https://github.com/xtuple/nvm.git $NVM_DIR  
+  ln -s /usr/local/nvm/nvm_bin.sh /usr/local/bin/nvm
 fi
 
-echo
-
-# Detect profile file, .bash_profile has precedence over .profile
-if [ ! -z "$1" ]; then
-  PROFILE="$1"
-else
-  if [ -f "$HOME/.bash_profile" ]; then
-	PROFILE="$HOME/.bash_profile"
-  elif [ -f "$HOME/.zshrc" ]; then
-  	PROFILE="$HOME/.zshrc"
-  elif [ -f "$HOME/.profile" ]; then
-	PROFILE="$HOME/.profile"
-  fi
-fi
-
-SOURCE_STR="[ -s \$HOME/.nvm/nvm.sh ] && . \$HOME/.nvm/nvm.sh  # This loads NVM"
-
-if [ -z "$PROFILE" ] || [ ! -f "$PROFILE" ] ; then
-  if [ -z $PROFILE ]; then
-	echo "=> Profile not found. Tried $HOME/.bash_profile and $HOME/.profile"
-  else
-	echo "=> Profile $PROFILE not found"
-  fi
-  echo "=> Run this script again after running the following:"
-  echo
-  echo "\ttouch $HOME/.profile"
-  echo
-  echo "-- OR --"
-  echo
-  echo "=> Append the following line to the correct file yourself"
-  echo
-  echo "\t$SOURCE_STR"
-  echo
-  echo "=> Close and reopen your terminal afterwards to start using NVM"
-  exit
-fi
-
-if ! grep -qc 'nvm.sh' $PROFILE; then
-  echo "=> Appending source string to $PROFILE"
-  echo "" >> "$PROFILE"
-  echo $SOURCE_STR >> "$PROFILE"
-else
-  echo "=> Source string already in $PROFILE"
-fi
-
-echo "=> Close and reopen your terminal to start using NVM"
+echo "NVM installed"
---
+++ nvm_bin.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export NVM_DIR=/usr/local/nvm
+. /usr/local/nvm/nvm.sh
+nvm $@
